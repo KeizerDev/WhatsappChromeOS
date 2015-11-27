1d07ca6b1db63eb5bcf420ccb7fd237db748c52d
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-11-27 13:12:32 +0100

    Added everything

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,14 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-  var h1 = document.getElementsByTagName('h1');
-  if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + ' \'Allo';
-  }
-}, false);
+$(function() {
+    adview();
+    $('.overlay').on('click', ' .close', function(event) {
+        $('.overlay').removeClass('visable');
+        adview();
+        event.preventDefault();
+    });
+});
+
+function adview() {
+    setTimeout(function() {
+        $('.overlay').addClass('visable');
+    }, 2000000);
+}
